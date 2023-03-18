8c478ad2566d00685831291d1db32175422c6985
GichohiWaweru <2003gichohiwaweru>
2023-03-18 12:48:25 +0300

    The dropdown tables

---
+++ Home.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_SESSION))
+    {
+        session_start();
+    }
+        ?>
+        
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <title>FOURFRONT TECHNOLOGIES</title>
+        <link href="home.css" rel="stylesheet" type="text/css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <style type="text/css">
+        <!--
+        .style8 {font-size: 24px}
+        .style9 {font-size: 95%; font-weight: bold; color: #003300; font-family: Verdana, Arial, Helvetica, sans-serif; }
+        -->
+        </style>
+        </head>
+        <body>
+        <div class="dropdown">
+          <button class="dropbtn">Dropdown</button>
+          <div class="dropdown-content">
+            <a href="">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        </body>
+        </html>
