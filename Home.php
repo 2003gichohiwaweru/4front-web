d9f37dafb39d4b3d14bcc3c81e578d21081d13b4
GichohiWaweru <2003gichohiwaweru>
2023-03-24 01:21:51 +0300

    changes

--- Home.php
+++ Home.php
@@ -1,79 +1,53 @@
 
-<?php
-if(isset($_SESSION))
-    {
-        session_start();
-    }
-        ?>
-        
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-        <title>FOURFRONT TECHNOLOGIES</title>
-        <link href="home.css" rel="stylesheet" type="text/css" />
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-        <style type="text/css">
-        <!--
-        .style8 {font-size: 24px}
-        .style9 {font-size: 95%; font-weight: bold; color: #003300; font-family: Verdana, Arial, Helvetica, sans-serif; }
-        -->
-        </style>
-        </head>
-        <body>
-        <nav>
-            <button class="dropbtn"></button>
-                <div class="men">
-                    <ul>
-                        <li><a href="HOME.html">CONTACTS</li>
-                        <li><a href="SHOP.html"> SHOP</a></li>
-                        <li> <a href="UPDATES.html">UPDATES</a></li>
-                        <li><a href="CONTACTS.html">CONTACTS</li>
-                    </ul>
-              
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>FOURFRONT COMPUTERS</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style type="text/css">
+<!--
+.style8 {font-size: 24px}
+.style9 {font-size: 95%; font-weight: bold; color: #003300; font-family: Verdana, Arial, Helvetica, sans-serif; }
+-->
+</style>
+</head>
+<body>
+<div id="wrapper">
 
-                </div>
-        </nav>
-        <h1>FOURFRONT COMPUTERS</h1>
-       
-    
-        <style>
-        body{
-            background-color: white;
-            background-image: url(C:\xampp\htdocs\4front-web\Media);
-            display: block;
-        
-        
-        }
-         .men{
-  
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: larger;          
-            background-position-x: 15px;
-            text-align: center;
-        }
-        .men ul {
-            display:inline-flex;
-            list-style: none;
-            list-style-type: none;
-            color:grey;
-        }
-        .men ul li {
-            width: 50;
-            margin: 15px;
-            padding: 15px;
-            background-color:none;
-            box-sizing: none;
-        }
-        h1 {
-                margin: 50px;
-                letter-spacing: 12px;
-                padding: 80px;
-                text-align: top;
-            
-                color:red;
-                font-family: inherit;
-                font-size: 40px;
-}
-        </style>
-        </body>
-        </html>
+  <?php
+  include "Header.php";
+  ?>
+
+  <div id="content">
+    <h2><span style="color:#003300"> THE FOURFRONT COMPUTERS SOLUTIONS</span></h2>
+    <table width="100%" border="0" cellspacing="3" cellpadding="3">
+      <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><p><img src="Media/admin.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+        <td><p><img src="Media/Hp Laptop.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+        <td><p><img src="Media/oppo.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+      </tr>
+      <tr>
+        <td height="26" bgcolor="#BCE0A8"><div align="center" class="style9">OPPO</div></td>
+        <td bgcolor="#BCE0A8"><div align="center" class="style9">HP</div></td>
+        <td bgcolor="#BCE0A8"><div align="center" class="style9">DELL</div></td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+  </div>
+ /*<?php
+ include "Right.php";
+ ?>
+ */
+  <div style="clear:both;"></div>
+   <?php
+ include "Footer.php";
+ ?>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<div id="title">
+    <h1><span style="color: #008000;">FOURFRONT COMPUTERS</span></h1>
+  </div>
+  
+<div class="container">
+    <ul id="navCircle">
+     
+
+      <li><a href="Category.php">HOME</a></li>
+      <li><a href="Products.php">SHOPS</a></li>
+      <li><a href="Offers.php">UPDATES</a></li>
+      <li><a href="Register.php">CART</a></li>
+      <li><a href="Contact.php">CONTACTS</a></li>
+    
+    </ul>
+  </div>
