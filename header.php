09f659ad7aec7e447d41e4bcdf213889b414690a
GichohiWaweru <2003gichohiwaweru>
2023-03-29 14:58:10 +0300

    uifhdygqh\ar

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
      
 
       <li><a href="Category.php">HOME</a></li>
-      <li><a href="Products.php">SHOPS</a></li>
+      <li><a href="Products.php">SHOP</a></li>
       <li><a href="Offers.php">UPDATES</a></li>
       <li><a href="Register.php">CART</a></li>
       <li><a href="Contact.php">CONTACTS</a></li>
